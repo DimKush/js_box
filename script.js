dd3f8780bae361d00c4c65ba236ebd8c5d867079
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-01 19:27:54 +0300

    dive into js basic project

---
+++ script.js
@@ -0,0 +1,13 @@
+var message = "Simple message!"
+console.log(message)
+
+var num = 42 + '2'
+var str = 'str'
+str = {a : 22 }
+var logic = true
+var obj = { a : 1 }
+var isNull = null
+var undf = undefined
+
+
+console.log(typeof str)
