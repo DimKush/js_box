0000cf5c6678a55d8f86db0d3402aa6ab483bf2e
DimKush <89531132041Dom>
2021-04-02 21:09:02 +0300

    Continue sandbox

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var message = "Simple message!"
-console.log(message)
-
-var num = 42 + '2'
-var str = 'str'
-str = {a : 22 }
-var logic = true
-var obj = { a : 1 }
-var isNull = null
-var undf = undefined
+var carYear = 2008;
+var personYear = 1990;
+var compareAge = new Number(20)
 
 
-console.log(typeof str)
+function checkAge(year){
+    return new Date().getFullYear() - year;
+}
+
+function returnPhrase(resultAge, compareAge){
+    return resultAge > compareAge ? " is bigger" : "is less";
+}
+
+function outputWarning(objType, compareAge, year){
+    objType = new String(objType).toLowerCase();
+
+    console.log("object = " + objType, "year : " + year, returnPhrase(checkAge(year),compareAge) + " that " + compareAge);   
+}
+
+outputWarning("PeRsON", compareAge,personYear);
+outputWarning("cAr", compareAge,carYear);
+
+////////////////
+// drop vowels
+
+function reverseString(str){
+    console.log("basic string : ", str);
+    
+    var chr = undefined;
+    for(var i = 0, j = str.length; i <= str.length && j >= 0; i++, j--){
+        chr = str.indexOf(i);
+        str.charAt(i) = str.charAt(j)
+        str.charAt(j) = chr;
+    }
+
+    console.log("reversed string", str);
+}
+
+
+var str = new String("String value");
+
+reverseString(str);
