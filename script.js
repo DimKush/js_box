0c661484082bb04cafe0e6f258d3bbba152f6a5b
DimKush <demonmadman228@gmail.com>
2021-05-24 21:23:20 +0300

    01-jsbox | difference between var and let

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var personName = 'Peter';
+let personName = 'Peter';
 
 var message = 'Hi \''+ personName + '\'';
 
 console.log(message);
 
+function pp(){
+    let val = 'Simple';
+    console.log(personName);
+    console.log(val);
+}
+
+// console.log(val);
