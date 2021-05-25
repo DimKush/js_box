dd4ffaf91dd371aea95d53873c961d1b1baac40a
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-05-25 22:10:23 +0300

    js-box | install linter EsLint

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var personName = 'Peter';
 
 var message = 'Hi \''+ personName + '\'';
 
-console.log(message);
+console.log(message); 
 
+let num = '0';
+console.log(Boolean(num));
+
+console.log(17 === '17');
+
+console.log(undefined == null);
+console.log(undefined === null);
