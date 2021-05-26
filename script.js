cbf61621b808ab482956fd9618ae510d112d7aa0
DimKush <dimkysh@gmail.com>
2021-05-26 22:30:01 +0300

    js-box | array as object and as literal expression

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var personName = 'Peter';
 
 var message = 'Hi \''+ personName + '\'';
 
-console.log(message);
+//arrow function 
+let getSquare = (num) => num * num;
 
+
+let arr = [10,230,23];
+let arr2 = new Array(12,23,44,55);
+
+const res =  getSquare(5);
+console.log(res);
