d55eb70b0bb6d9cb393856b2b4dc465f6621ea11
DimKush <dimkysh@gmail.com>
2021-06-05 17:11:54 +0300

    js-box | sort contacts by name

--- strings.js
+++ strings.js
@@ -9,5 +9,29 @@ const contacts = [
         name: "John",
         phoneNumber: "555-55-55",
     },
-
+    {
+        name: "Billy",
+        phoneNumber: "434-55-66"
+    },
+    {
+        name: "Ann",
+        phoneNumber: "563-34-55",
+    },
+    {
+        name: "Hardy",
+        phoneNumber: "563-34-66",
+    },
+    {
+        name: "Michael",
+        phoneNumber: "233-23-55-56"
+    },
 ]
+
+const sortContactsByName = contacts => {
+    return contacts.sort(function(a,b){
+        return a.name > b.name ? 1 : -1;
+    });
+}
+
+console.log(sortContactsByName(contacts));
+
