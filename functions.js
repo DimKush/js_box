26fd1d911a263cc0f82799281e5ebb41bacd33a8
DimKush <dimkysh@gmail.com>
2021-06-06 21:11:56 +0300

    js-box | include babel and search begin Js functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+
+
+
+function sendMessage(){
+    const name = "John";
+    const senderName = "Dim";
+    console.log(`Hello ${name} from ${senderName}`);
+}
+
+sendMessage();
---
+++ strings/strings.test.js
@@ -0,0 +1,11 @@
+
+import { simpleCalculator } from './strings.js'
+
+
+
+let ten = 10
+
+it('Calculator unit test', () =>{
+    let res = simpleCalculator("1 + 3");
+    expect("1 + 3 = 4")
+});
