797e29733df23c23147cd7c68a555416d31a01bf
DimKush <dimkysh@gmail.com>
2021-06-07 23:05:05 +0300

    js-box | escape character

--- functions.js
+++ functions.js
@@ -5,6 +5,7 @@ function sendMessage(){
     const name = "John";
     const senderName = "Dim";
     console.log(`Hello ${name} from ${senderName}`);
+    console.log(`${name}\n fallow.`)
 }
 
 sendMessage();
