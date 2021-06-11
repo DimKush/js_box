7644c38719f065721abf6daee2b64abe1686a680
DimKush <dimkysh@gmail.com>
2021-06-11 19:01:34 +0300

    js-box | difference between var and let/const variables

--- hoisting.js
+++ hoisting.js
@@ -4,7 +4,7 @@ const f = () =>{
 	var a = 1;
 	console.log(a);
 }
-
+ 
 if(a){
 	var a =1;
 	console.log(a);
@@ -12,3 +12,19 @@ if(a){
 
 f();
 console.log(a);
+
+
+let b = 23;
+
+const fo = () =>{
+	let b = 2;
+	console.log(b);
+}
+ 
+if(b){
+	let b =3;
+	console.log(b);
+}
+
+fo();
+console.log(b);
