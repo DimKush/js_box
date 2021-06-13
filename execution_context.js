c433aa5f84c1654310e165a0202acffd69d5021d
DimKush <dimkysh@gmail.com>
2021-06-13 18:45:02 +0300

    js-box | call and apply messages for execution context

--- execution_context.js
+++ execution_context.js
@@ -5,6 +5,9 @@ const user = {
 	age : 18,
 	sayMessage(message) {
 		console.log(`${this.name} aged : ${this.age}, sayed "${message}" to you.`)
+	},
+	helloFromPerson(){
+		console.log(`${this.name}, aged ${this.age} says: "Hello!".`);
 	}
 };
 
@@ -16,6 +19,11 @@ user.walk();
 user.sayMessage(`simple words`);
 
 const func = user.sayMessage.bind(user, `bind simple`);
-// . bind();
-
+const funcSec = user.helloFromPerson;
+const funcThird = user.sayMessage;
 func();
+
+
+funcSec.call({name: "Bob", age : 23});
+
+funcThird.apply({ name: "Kate", age : 24 }, ["Message from apply"]);
