3fcf9935a93caa8d1544820697e401ebcfe8a774
DimKush <dimkysh@gmail.com>
2021-06-20 19:43:29 +0300

    js-box | plus few days

--- date.js
+++ date.js
@@ -1,3 +1,14 @@
 'use strict'
 const today = new Date();
 console.log(today);
+
+const getDayOfWeek = (date, days) => {
+	const day = new Date(date).getDate();
+	const dateInFuture = new Date(date).setDate(day + days);
+
+	return dateInFuture;
+};
+
+const res = getDayOfWeek(new Date(), 25);
+
+console.log(new Date(res));
