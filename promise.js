e2aa91491e0ad066cbe86c51c484b5ef8636df60
DimKush <dimkysh@gmail.com>
2021-06-25 21:26:14 +0300

    js-box | promises in js

---
+++ promise.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const addImageWithPromise = imgPath => {
+	const prm = new Promise((resolve, reject) => {
+		const imgElement = document.createElement('img');
+		imgElement.setAttribute('alt', 'MyPhoto');
+		imgElement.src = imgPath;
+		const containerElem = document.querySelector('.page');
+
+		containerElem.append(imgElement);
+
+		const imgLoaded = () => {
+			const {width,height} = imgElement;
+			resolve({width,height});
+		}
+
+		imgElement.addEventListener('load', imgLoaded);
+
+		imgElement.addEventListener('error', () => reject( new Error('Image load failed.')));
+	}); 
+
+	return prm;
+}
+
+const imgPathOrig = `https://free4kwallpapers.com/uploads/originals/2020/03/30/cyberpunk-wallpaper.jpg`;
+
+const prmObj = addImageWithPromise(imgPathOrig);
+
+console.log(prmObj);
